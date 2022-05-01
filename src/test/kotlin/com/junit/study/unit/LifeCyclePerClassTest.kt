ddfa28cff30f6b77fdb2bc2aa51de6494a99e7f1
ktj1997 <rlaxowns7916@gmail.com>
2022-05-01 17:49:39 +0900

    feat: Test Instance LifeCycle

---
+++ src/test/kotlin/com/junit/study/unit/LifeCyclePerClassTest.kt
@@ -0,0 +1,64 @@
+package com.junit.study.unit
+
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.MethodOrderer
+import org.junit.jupiter.api.Order
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.TestMethodOrder
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+/**
+ * LifeCycle.PER_CLASS 일 때는
+ * 테스트간의 인스턴스를 공유하며 하나의 인스턴스만을 사용한다.
+ */
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation::class)
+class LifeCyclePerClassTest {
+    companion object {
+        val logger: Logger = LoggerFactory.getLogger(this.javaClass)
+    }
+
+    var counter: Int = 1
+
+    /**
+     * LifeCycle.PER_CLASS 일 때는
+     * @BeforeAll @AfterAll 이 Static일 필요가 없어진다.
+     */
+    @BeforeAll
+    fun beforeAll() {
+        logger.info("Before All")
+    }
+
+    @AfterAll
+    fun afterAll() {
+        logger.info("After All")
+    }
+
+    @Test
+    @Unit
+    @Order(1)
+    fun firstCounterTest() {
+        logger.info(this.toString())
+        assertEquals(counter++, 1)
+    }
+
+    @Test
+    @Unit
+    @Order(2)
+    fun secondCounterTest() {
+        logger.info(this.toString())
+        assertEquals(counter++, 2)
+    }
+
+    @Test
+    @Unit
+    @Order(3)
+    fun thirdCounterTest() {
+        logger.info(this.toString())
+        assertEquals(counter++, 3)
+    }
+}
---
+++ src/test/kotlin/com/junit/study/unit/LifeCyclePerMethodTest.kt
@@ -0,0 +1,45 @@
+package com.junit.study.unit
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+/**
+ * Junit의 인스턴스 기본전략은 PER_METHOD
+ * Mehotd를 실행 할 때 마다 다른 인스턴스가 사용된다.
+ * 그렇기에 테스트 순서에서 자유롭다.
+ * 테스트간의 의존성을 없애기 위함이다.
+ */
+class LifeCyclePerMethodTest {
+    companion object {
+        val logger: Logger = LoggerFactory.getLogger(this.javaClass)
+    }
+    var counter: Int = 1
+
+    /**
+     * 테스트 메소드마다 다른 인스턴스를 사용하고 있기 때문에
+     * counter의 값이 공유되지 않는다.
+     */
+
+    @Unit
+    @Test
+    fun firstCounterTest() {
+        logger.info(this.toString())
+        assertEquals(counter++, 1)
+    }
+
+    @Unit
+    @Test
+    fun secondCounterTest() {
+        logger.info(this.toString())
+        assertEquals(counter++, 1)
+    }
+
+    @Unit
+    @Test
+    fun thirdCounterTest() {
+        logger.info(this.toString())
+        assertEquals(counter++, 1)
+    }
+}
