9e11e7fd90696dfca848f1e61f915c245876ef43
ktj1997 <rlaxowns7916@gmail.com>
2022-05-02 19:31:03 +0900

    feat: docker-compose.yml

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,9 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation ("org.testcontainers:testcontainers:1.16.3")
+    testImplementation ("org.testcontainers:junit-jupiter:1.16.3")
+
 }
 
 tasks.withType<KotlinCompile> {
